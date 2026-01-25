5cec338f077f42255062647fe665b5347ff31bb9
github-action-benchmark <github@users.noreply.github.com>
2026-01-25 12:42:00 +0000

    add Benchmark (pytest) benchmark result for b44d6e9cea0c3e22be2b46146ecb9dbf98a542cc

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1769344920311,
+  "repoUrl": "https://github.com/ozlabsai/gatling",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "guy.na8@gmail.com",
+            "name": "Clem 🤗",
+            "username": "GuyNachshon"
+          },
+          "committer": {
+            "email": "guy.na8@gmail.com",
+            "name": "Clem 🤗",
+            "username": "GuyNachshon"
+          },
+          "distinct": true,
+          "id": "b44d6e9cea0c3e22be2b46146ecb9dbf98a542cc",
+          "message": "feat: init",
+          "timestamp": "2026-01-25T14:40:38+02:00",
+          "tree_id": "b1cf8d100dc7c0a2e358c3710a01c08da5a01d2c",
+          "url": "https://github.com/ozlabsai/gatling/commit/b44d6e9cea0c3e22be2b46146ecb9dbf98a542cc"
+        },
+        "date": 1769344919729,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test/test_encoders/test_governance_encoder.py::TestPerformance::test_inference_latency",
+            "value": 2.316045288931587,
+            "unit": "iter/sec",
+            "range": "stddev: 0.059745371249583165",
+            "extra": "mean: 431.7704859999992 msec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
